ff457ee16fd7143d9aed5df8eed4cb0644a85f23
TaylorSpencer22 <taylorspencer522@gmail.com>
2025-08-24 00:36:01 -0500

    -

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <>
      <p>well, you're here because you're broke.</p>
      <p>let's find you a cheap flight :'</p>
+     <p>let's find you a cheap flight :'</p>
     </>
   )
 }
